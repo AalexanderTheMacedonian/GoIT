94336f6536dc84b1edf3b4c4b6a2348b04e0e60e
AlexandrTheMacedonian <Sanyasemenov96@gmail.com>
2016-10-29 15:22:33 +0300

    Initial commit

---
+++ src/Module1/Practice/Main.java
@@ -0,0 +1,10 @@
+package Module1.Practice;
+
+/**
+ * Created by alexandrsemenov on 29.10.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
