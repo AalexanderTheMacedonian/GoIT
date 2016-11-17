01ed9387c205df0669ecaa7218188ab968890146
AlexandrTheMacedonian <Sanyasemenov96@gmail.com>
2016-11-17 16:10:52 +0200

    add homework4

---
+++ src/module4/Test.java
@@ -0,0 +1,7 @@
+package module4;
+
+/**
+ * Created by alexandrsemenov on 16.11.16.
+ */
+public class Test {
+}
---
+++ src/module4/homework/Currency.java
@@ -0,0 +1,8 @@
+package module4.homework;
+
+/**
+ * Created by alexandrsemenov on 16.11.16.
+ */
+public enum Currency {
+    USD, EUR
+}
