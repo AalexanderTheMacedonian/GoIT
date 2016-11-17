b46cb46539986cb8ddc8879dd3a6acddcfe40666
AlexandrTheMacedonian <Sanyasemenov96@gmail.com>
2016-11-17 17:35:17 +0200

    New changelist

--- src/Practice/lesson3/task3/Person.java
+++ src/Practice/lesson3/task3/Person.java
@@ -3,11 +3,9 @@ package Practice.lesson3.task3;
 /**
  * Created by alexandrsemenov on 14.11.16.
  */
-public class Person {
-    Boolean isMale;
+public abstract class Person {
+    Gender gender;
     String name;
     int age;
     String secondName;
-
-
 }
---
+++ src/Practice/lesson3/task3/Worker.java
@@ -0,0 +1,30 @@
+package Practice.lesson3.task3;
+
+/**
+ * Created by alexandrsemenov on 17.11.16.
+ */
+public class Worker {
+    private String adres;
+    private long salary;
+
+    public Worker(String adres, long salary) {
+        this.adres = adres;
+        this.salary = salary;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+}
